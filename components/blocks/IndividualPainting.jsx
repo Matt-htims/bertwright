536e88dc91b3052468c185202bbe968c773a2785
Matt Smith <matt.smith3108@gmail.com>
2023-01-06 14:22:02 +0000

    Basic styling of IndividualPainting page as well as getting the return to previous page link to work properly with router

--- components/blocks/IndividualPainting.jsx
+++ components/blocks/IndividualPainting.jsx
@@ -1,17 +1,36 @@
 import Image from 'next/image';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
+import { FiArrowLeft } from 'react-icons/fi';
 
 export default function IndividualPainting({ painting }) {
+	const router = useRouter();
+	console.log(router.asPath, router.query.slug);
+
+	const returnLink = router.asPath.replace(router.query.slug, '');
+
 	return (
-		<div>
-			<Image
-				src={painting.src}
-				width="1000"
-				height="1000"
-				alt={painting.title}
-			/>
-			<div>
-				<h1>{painting.title}</h1>
-				<p>{painting.description}</p>
+		<div className="pb-0">
+			<div className="h-[calc(100vh-80px)] relative overlow-auto pt-8">
+				<Link className="flex items-center space-x-2 mb-1" href={returnLink}>
+					<FiArrowLeft />
+					<p className="text-sm">Back to previous page</p>
+				</Link>
+				<div className="h-full border-top">
+					<div className=" h-[80%] flex center relative">
+						<Image
+							src={painting.src}
+							fill
+							alt={painting.title}
+							className="object-contain py-8"
+						/>
+					</div>
+					<div className="text-center space-y-3 max-w-xl m-auto">
+						<h1 className="text-4xl font-medium">{painting.title}</h1>
+						<p className="text-base font-light">{painting.description}</p>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
